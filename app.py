3cce811093b3d9ca2672042d885b2b6e35093dc5
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2021-06-28 19:05:21 +0700

    no browser cache

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import os
 import imageio
 import argparse
 import requests
+import cv2
+import numpy as np
 from pathlib import Path
 from werkzeug.utils import secure_filename
 from modules import get_prediction
@@ -22,6 +24,7 @@ parser.add_argument('--host',  type=str, default='192.168.100.4', help="Local IP
 
 
 app = Flask(__name__, template_folder='templates', static_folder='assets')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 UPLOAD_FOLDER = './assets/uploads'
 DETECTION_FOLDER = './assets/detections'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -41,12 +44,18 @@ def homepage():
 def analyze():
     f = request.files['file']
 
+    ori_file_name = secure_filename(f.filename)
+    _, ext = os.path.splitext(ori_file_name)
+
     # Get cache name by hashing image
-    filename = hashlib.md5(f.read()).hexdigest() + '.jpg'
+    data = f.read()
+    filename = hashlib.md5(data).hexdigest() + f'{ext}'
     
     # save file to /static/uploads
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    f.save(filepath)
+    np_img = np.fromstring(data, np.uint8)
+    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    cv2.imwrite(filepath, img)
 
     # predict image
     output_path=os.path.join(app.config['DETECTION_FOLDER'], filename)
@@ -55,11 +64,16 @@ def analyze():
         output_path, 
         model_name="yolov5m",
         ensemble=False,
-        min_conf=0.1,
+        min_conf=0.5,
         min_iou=0.65)
 
-    return render_template("detect.html", fname=filename, fname2=filename2, result_dict=result_dict)
+    return render_template("detect.html", fname=filename, fname2=filename, result_dict=result_dict)
 
+@app.after_request
+def add_header(response):
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store'
+    return response
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
