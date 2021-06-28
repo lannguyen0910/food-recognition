a36629bd612c2d03d0a5866c3338d507786b8562
lannguyen <18120051@student.hcmus.edu.vn>
2021-06-28 09:21:32 +0700

    support upload .mp4 file

--- static/js/client.js
+++ static/js/client.js
@@ -8,16 +8,35 @@ function clear() {
   $('#image-display').empty(); // removes previous img
   $('#upload-label').empty(); //removes previous img name
   $('#result-content').empty();   //remove result content (image + labels ...)
-  $('canvas').empty();
+
+  // let canvas = document.querySelector("canvas");
+  // const context = canvas.getContext('2d');
+  // context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function showPicked(input) {
   clear();
   el("upload-label").innerHTML = input.files[0].name;
+  var extension = input.files[0].name.split(".")[1].toLowerCase();
   var reader = new FileReader();
   reader.onload = function(e) {
-      el("image-display").src = e.target.result;
-      el("image-display").className = "";
+    var file_url = e.target.result
+    if (extension === "mp4"){
+      var video_html = '<video autoplay id="user-video" controls> <source id="user-source"></source></video>'
+      $('#image-display').html(video_html); // replaces previous video
+      var video = el("user-video");
+      var source = el("user-source");
+      source.setAttribute("src", file_url);
+      video.load();
+      video.play();
+    }
+
+    else if(extension === "jpg" || extension === "jpeg" || extension === "png"){
+      var img_html = '<img id="user-image" src="' + file_url + '" style="display: block;margin-left: auto;margin-right: auto;width: 640px; height: 480px"/>';
+      $('#image-display').html(img_html); // replaces previous img
+    }
+
+    $('#webcam-video').prop('disabled', true); //disable image upload
   };
   reader.readAsDataURL(input.files[0]);
 }
@@ -93,7 +112,7 @@ function runWebcam() {
 
       btnNewPhoto.onclick = takeAPhoto;
       btnDownload.onclick = downloadPhoto;
-      
+
 
       videoCamera.onloadedmetadata = function () {
         videoCamera.setAttribute("width", this.videoWidth);
@@ -101,7 +120,7 @@ function runWebcam() {
         canvasPhoto.setAttribute("width", this.videoWidth);
         canvasPhoto.setAttribute("height", this.videoHeight);
         videoCamera.play();
-
+        
       };
     })
     .catch(function (err) {
@@ -136,6 +155,6 @@ window.onload = function(){
 
   $('#confidence-range').on('input', function() {
     $('#confidence-text span').html(this.value);
-    threshold = $('#confidence-range').val() / 100;
+    confidence = $('#confidence-range').val() / 100;
   });
 }
