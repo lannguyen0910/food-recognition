e26d28f4c566aafff94f6a5267460f6e4e161dcb
lannguyen <18120051@student.hcmus.edu.vn>
2021-06-29 04:00:59 +0700

    add check valid url (image + youtube video)

--- static/js/client.js
+++ static/js/client.js
@@ -176,7 +176,10 @@ function downloadPhoto() {
   }, "image/jpeg", 1);
 };
 
+
 // URL handle
+
+// image url
 var detectURLBtn = document.querySelector("#url-button");
 
 function isValidHttpUrl(string) {
@@ -188,11 +191,56 @@ function isValidHttpUrl(string) {
     return false;  
   }
 
-  return (url.protocol === "http:" || url.protocol === "https:") && string.match(/\.(jpeg|jpg|gif|png)$/) != null;
+  return (url.protocol === "http:" || url.protocol === "https:");
+}
+
+function imageExists(image_url){ //https://stackoverflow.com/questions/18837735/check-if-image-exists-on-server-using-javascript
+    var http = new XMLHttpRequest();
+
+    http.open('HEAD', image_url, false);
+    http.send();
+
+    return http.status != 404;
+}
+
+// youtube url
+function YouTubeGetID(url){ //https://gist.github.com/takien/4077195
+  var ID = '';
+  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if(url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\-]/i);
+    ID = ID[0];
+  }
+  
+  else {
+    ID = url;
+  }
+  
+  return ID;
+}
+
+function validVideoId(id) { //https://gist.github.com/tonY1883/a3b85925081688de569b779b4657439b
+		var img = new Image();
+    var load = null;
+		img.src = "http://img.youtube.com/vi/" + id + "/mqdefault.jpg";
+		img.onload = function () {
+			load = checkThumbnail(this.width);
+      
+      return load;
+    }
+	}
+
+function checkThumbnail(width) {
+  //HACK a mq thumbnail has width of 320.
+  //if the video does not exist(therefore thumbnail don't exist), a default thumbnail of 120 width is returned.
+  if (width === 120) {
+    return false;
+  }
+  return true;
 }
 
 function trackURL(url){
-  if (isValidHttpUrl(url)){
+  if (isValidHttpUrl(url) && (imageExists(url) || validVideoId(YouTubeGetID(url)))){
     detectURLBtn.removeAttribute("disabled");
   }
   else{
