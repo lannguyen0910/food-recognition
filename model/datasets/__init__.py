16d24f0b0f965ccf6301c694269e296cc226a172
kaylode <kaylode@github.com>
2021-07-01 20:26:35 +0700

    add download file

---
+++ model/datasets/__init__.py
@@ -0,0 +1,21 @@
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+
+food_list = [
+    "Apple", "Orange", "Pizza", "Hamburger", "French fries", 
+    "Sandwich", "Baked goods", "Cheese", "Burrito", "Banana",
+    "Cake", "Pancake", "Coffee", "Tea", "Milk", "Salab",
+    "Cucumber", "Tomato", "Egg"]
+
+dataset = foz.load_zoo_dataset(
+    "open-images-v6",
+    split="validation",
+    label_types=["detections"],
+    classes = food_list,
+    max_samples=100,
+    seed=51,
+    shuffle=True,
+    dataset_name="open-images-food",
+)
+
