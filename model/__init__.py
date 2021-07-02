8508c79a498ed07c5dcdb5e731f3444ef96cfb7f
kaylode <kaylode@github.com>
2021-07-03 00:02:24 +0700

    add classifiers

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 from .detect import detect
+from .classify import classify
 from .configs import Config
 from .trainer import get_config
 from .utils import download_weights, draw_boxes_v2, get_class_names, postprocessing, box_fusion
-from .models import get_classification_predict
