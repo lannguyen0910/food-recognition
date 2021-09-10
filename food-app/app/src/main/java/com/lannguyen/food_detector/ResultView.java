aeefdf78c92d2596c337b54dc015f236e80b70be
lannguyen <18120051@student.hcmus.edu.vn>
2021-09-10 17:00:28 +0700

    result view

---
+++ food-app/app/src/main/java/com/lannguyen/food_detector/ResultView.java
@@ -0,0 +1,64 @@
+package com.lannguyen.food_detector;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class ResultView extends View {
+
+    private final static int TEXT_X = 40;
+    private final static int TEXT_Y = 35;
+    private final static int TEXT_WIDTH = 260;
+    private final static int TEXT_HEIGHT = 50;
+
+    private Paint mPaintRectangle;
+    private Paint mPaintText;
+    private ArrayList<Result> mResults;
+
+    public ResultView(Context context) {
+        super(context);
+    }
+
+    public ResultView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        mPaintRectangle = new Paint();
+        mPaintRectangle.setColor(Color.YELLOW);
+        mPaintText = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mResults == null) return;
+        for (Result result : mResults) {
+            mPaintRectangle.setStrokeWidth(5);
+            mPaintRectangle.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(result.rect, mPaintRectangle);
+
+            Path mPath = new Path();
+            RectF mRectF = new RectF(result.rect.left, result.rect.top, result.rect.left + TEXT_WIDTH,  result.rect.top + TEXT_HEIGHT);
+            mPath.addRect(mRectF, Path.Direction.CW);
+            mPaintText.setColor(Color.MAGENTA);
+            canvas.drawPath(mPath, mPaintText);
+
+            mPaintText.setColor(Color.WHITE);
+            mPaintText.setStrokeWidth(0);
+            mPaintText.setStyle(Paint.Style.FILL);
+            mPaintText.setTextSize(32);
+            canvas.drawText(String.format("%s %.2f", PrePostProcessor.mClasses[result.classIndex], result.score), result.rect.left + TEXT_X, result.rect.top + TEXT_Y, mPaintText);
+        }
+    }
+
+    public void setResults(ArrayList<Result> results) {
+        mResults = results;
+    }
+}
