e09c3c0ef2900c2ad963d32ac8f8b4a5722cff52
lannguyen <18120051@student.hcmus.edu.vn>
2021-12-23 12:01:56 +0700

    update food database

--- api/api.py
+++ api/api.py
@@ -1,9 +1,10 @@
 import json
 import requests
 from tqdm import tqdm
-from .secret import API, get_response
+from secret import API, get_response
 
 DATABASE = "./api/db.json"
+
 
 def make_request(api_name, params, headers):
     api_dict = API[api_name]
@@ -13,23 +14,24 @@ def make_request(api_name, params, headers):
     input_params = {}
     input_params.update(api_auth)
     input_params.update(params)
-    
+
     response = requests.get(
-        api_url, 
-        params=input_params, 
+        api_url,
+        params=input_params,
         headers=headers)
 
-    result_dict = get_response(api_name,response)
+    result_dict = get_response(api_name, response)
     return result_dict
+
 
 def update_db(food_list, api_name="edamam"):
     query_str = API[api_name]['query_str']
-    headers = {"Accept": "application/json",}
+    headers = {"Accept": "application/json", }
     db = []
     for food_name in tqdm(food_list):
         query_str['ingr'] = food_name
         food_dict = make_request(
-            api_name, 
+            api_name,
             params=query_str,
             headers=headers)
 
@@ -37,8 +39,9 @@ def update_db(food_list, api_name="edamam"):
             db.append(food_dict)
         else:
             print(f"Failed to get {food_name}")
-    
+
     save_db(db)
+
 
 def save_db(db, out_name=DATABASE):
     # db: list[dict]
@@ -46,10 +49,11 @@ def save_db(db, out_name=DATABASE):
 
     with open(out_name, 'r') as f:
         data = json.load(f)
-    
+
     data['food'] += db
     with open(out_name, 'w') as f:
-        json.dump(data,f)
+        json.dump(data, f)
+
 
 def get_info_from_db(food_list):
     if not isinstance(food_list, list):
@@ -57,7 +61,7 @@ def get_info_from_db(food_list):
 
     with open(DATABASE, 'r') as f:
         data = json.load(f)
-    
+
     result_list = {
         "calories": [],
         "protein": [],
@@ -66,7 +70,7 @@ def get_info_from_db(food_list):
         "fiber": []
     }
     for food_name in food_list:
-        has_info=False
+        has_info = False
         for item in data['food']:
             if str.lower(food_name) == str.lower(item['name']):
                 for key in result_list.keys():
@@ -76,12 +80,14 @@ def get_info_from_db(food_list):
         if not has_info:
             for key in result_list.keys():
                 result_list[key].append(None)
-    
+
     return result_list
 
 
 if __name__ == '__main__':
     food_list = [
+        "broccoli",
+        'mushroom',
         'milk',
         'yogurt',
         'rice',
@@ -94,6 +100,7 @@ if __name__ == '__main__':
         'salad',
         'cherry tomatoe',
         'soup',
+        'soups',
         'tofu',
         'bibimbap',
         'fried noodles',
@@ -101,23 +108,19 @@ if __name__ == '__main__':
         'citrus',
         'apple',
         "banh mi",
-        "banh trang tron",
         "banh xeo",
         "bun bo hue",
         "bun dau",
         "com tam",
         "goi cuon",
         "pho",
-        "hu tieu",
-        "xoi",
-        "pizza",
         "hamburger",
+        "cabbage",
         "french fries",
         "sandwich",
+        'submarine sandwich',
         "cheese",
-        "burrito",
         "banana",
-        "pancake",
         "coffee",
         "tea",
         "cucumber",
@@ -133,6 +136,13 @@ if __name__ == '__main__':
         "takoyaki",
         "melitzanosalata",
         "carrot cake",
+        'carrot',
+        'common fig',
+        'grape',
+        'muffin',
+        'cocktail',
+        'zucchini',
+        'cooking spray',
         "apple pie",
         "zha jiang mian",
         "baklava",
@@ -142,6 +152,7 @@ if __name__ == '__main__':
         "sarma",
         "deviled eggs",
         "green curry",
+        'curry',
         "samosa",
         "chicken quesadilla",
         "french onion soup",
@@ -154,7 +165,15 @@ if __name__ == '__main__':
         "lobster bisque",
         "enchiladas",
         "moussaka",
-        "strawberry shortcake",
+        'strawberry',
+        'croissant',
+        'doughnut',
+        'fruit',
+        'grapefruit',
+        'honeycomb',
+        'lemon',
+        'lobster',
+        'mango',
         "pad thai",
         "ice cream",
         "greek salad",
@@ -164,7 +183,6 @@ if __name__ == '__main__':
         "spanakopita",
         "kolokythokeftedes",
         "lobster roll sandwich",
-        "hamsi",
         "croque madame",
         "risotto",
         "poutine",
@@ -174,8 +192,10 @@ if __name__ == '__main__':
         "naan",
         "souvlaki",
         "papdi chaat",
+        'squash',
         "fried calamari",
         "beef tartare",
+        'tart',
         "cig kofte",
         "coconut milk soup",
         "noodles with fish curry",
@@ -196,7 +216,6 @@ if __name__ == '__main__':
         "sushi",
         "french fries",
         "banh xeo",
-        "ispanak",
         "chocolate cake",
         "caprese salad",
         "beignets",
@@ -206,30 +225,29 @@ if __name__ == '__main__':
         "spring rolls",
         "beef in oyster sauce",
         "ramen",
-        "falafel",
         "hunkar begendi",
         "spaghetti bolognese",
         "salade nicoise",
         "blanquette de veau",
         "macaroni and cheese",
         "oysters",
+        'oyster',
         "artichoke bottoms in olive oil",
         "udon noodle",
         "hue beef rice vermicelli soup",
-        "goya champuru",
         "cheesecake",
         "paella",
         "fried rice",
-        "waffles",
         "stewed pork leg",
         "borek",
         "winter melon soup",
+        'vegetable',
         "hot and sour fish and vegetable ragout",
         "minestrone",
         "cheese plate",
-        "burrito",
         "foie gras",
         "chicken wings",
+        'wine',
         "baby back ribs",
         "gnocchi",
         "beet salad",
@@ -239,12 +257,22 @@ if __name__ == '__main__':
         "tomatokeftedes",
         "macarons",
         "tacos",
+        'taco',
         "spaghetti carbonara",
         "confit de canard",
         "fried mussel pancakes",
         "club sandwich",
         "caesar salad",
         "yellow curry",
+        "candy",
+        "cake",
+        'cantaloupe',
+        'pear',
+        'popcorn',
+        'pretzel',
+        'tomato',
+        'crab',
+        'lemon',
         "cao lau",
         "bruschetta",
         "khao soi",
@@ -270,9 +298,7 @@ if __name__ == '__main__':
         "bun cha",
         "cannoli",
         "filet mignon",
-        "soba noodle",
         "pulled pork sandwich",
-        "pancakes",
         "fried pork in scoop",
         "thai papaya salad",
         "kisir",
@@ -292,17 +318,86 @@ if __name__ == '__main__':
         "kung pao chicken",
         "dumplings",
         "charcoal-boiled pork neck",
-        "okinawa soba",
-        "tostadas",
         "malai kofta",
         "icli kofte",
         "tuna tartare",
         "panna cotta",
         "yemista",
         "red velvet cake",
+        'watermelon',
         "eggs benedict",
         "onion rings",
         "fish and chips",
+        "artichoke",
+        "asparagus",
+        "bagel",
+        "baked goods",
+        "beer",
+        "bell pepper",
+        'pasta',
+        'pastry',
+        'peach',
+        'pineapple',
+        'pomegranate',
+        'potato',
+        'pumpkin',
+        'radish'
     ]
 
-    update_db(food_list)
+    food_list_2 = [
+        "broccoli",
+        'mushroom',
+        'soups',
+        "cabbage",
+        'submarine sandwich',
+        'carrot',
+        'common fig',
+        'grape',
+        'muffin',
+        'cocktail',
+        'zucchini',
+        'cooking spray',
+        'curry',
+        'strawberry',
+        'croissant',
+        'doughnut',
+        'fruit',
+        'grapefruit',
+        'honeycomb',
+        'lemon',
+        'lobster',
+        'mango',
+        'squash',
+        'tart',
+        'oyster',
+        'vegetable',
+        'wine',
+        'taco',
+        "candy",
+        "cake",
+        'cantaloupe',
+        'pear',
+        'popcorn',
+        'pretzel',
+        'tomato',
+        'crab',
+        'lemon',
+        'watermelon',
+        'wintermelon',
+        "artichoke",
+        "asparagus",
+        "bagel",
+        "baked goods",
+        "beer",
+        "bell pepper",
+        'pasta',
+        'pastry',
+        'peach',
+        'pineapple',
+        'pomegranate',
+        'potato',
+        'pumpkin',
+        'radish'
+    ]
+
+    update_db(food_list_2)
