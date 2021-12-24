45b88f373f10b1fbf6f75d0b079ddce663e30dd0
lannguyen <18120051@student.hcmus.edu.vn>
2021-12-24 21:03:44 +0700

    update to conv operation

--- model/models/yolo/common.py
+++ model/models/yolo/common.py
@@ -509,7 +509,7 @@ class Focus(nn.Module):
         # self.contract = Contract(gain=2)
 
     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
-        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
+        return self.conv(torch.cat([x,x,x,x], 1))
         # return self.conv(self.contract(x))
 
 
