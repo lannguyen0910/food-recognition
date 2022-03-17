a0c9089296f2a9f4ce5ff6e2140546e7a0b04899
lannguyen <18120051@student.hcmus.edu.vn>
2022-03-17 22:10:57 +0700

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
+# Adapted from: https://github.com/serengil/deepface/blob/master/setup.py
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="theseus",
+    name="food-recognition",
     version='0.0.1',
+    author="Hoang-Lan Nguyen",
+    author_email="18120051@student.hcmus.edu.vn",
+    description="A Baseline Food Recognition using Theseus - A general template for most Pytorch projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lannguyen0910/food-recognition",
     packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
     python_requires='>=3.7',
     install_requires=[
         "numpy",
-        "torch",        
+        "torch",
         "gunicorn==20.1.0"
         "albumentations==0.5.2"
         "pyyaml>=5.1"
